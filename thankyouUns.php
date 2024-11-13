b2712e80e8b4b0c760485e7c5a47b6a9a1c7de44
Brumertz <nbrandon165@gmail.com>
2024-11-13 20:06:34 +0800

    spring 3
    added admin.php, signup.php, thankyou.php, thankyouUns.php

---
+++ thankyouUns.php
@@ -0,0 +1,11 @@
+<?php include 'includes/header.php'; ?>
+<div class="container-fluid d-flex justify-content-center align-items-center" style="min-height: 64vh;">
+<div>    
+    <h2>Hope See You Back!</h2>
+
+    <p>You have successfully Unsubscribe for the Acme Gallery Membership. You can signup  at any time through your email or contacting us directly.</p>
+    <a href="index.php" class="btn btn-secondary">Back to Home</a>
+
+    </div>   
+</div>
+<?php include 'includes/footer.php'; ?>
